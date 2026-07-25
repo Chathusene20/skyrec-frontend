435a1e34bd27b383c8fbb1b808b7ca10f569007d
Chathumini <chathuminisenethya246@gmail.com>
2026-07-26 00:27:15 +0530

    1

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -332,7 +332,7 @@ export default function UserData() {
                             </option>
 
 
-                            <option value="/settings">
+                            <option value="settings">
                                 Account Settings
                             </option>
 
