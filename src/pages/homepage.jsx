f05c1ebaa495669d57ff6b3a4dff7bd33f5def5c
Chathumini <chathuminisenethya246@gmail.com>
2026-08-18 18:57:22 +0530

    recorrected the account settings

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,11 +1,29 @@
-import Header from "../components/header";
+import { Route,Routes } from "react-router-dom"
+import Header from "../components/header"
+import { ProductPage } from "./productPage.jsx";
+import ProductOverview from "./productOverview.jsx"
+import CartPage from "./cart.jsx"
+import CheckoutPage from "./checkout.jsx"
 
-export default function HomePage() {
-    return (
+export default function HomePage(){
+     return (
         <div className="w-full h-full bg-primary">
-            <Header />
+               <Header/>
+               <Routes path="/">
+                 <Route path="/" element={<h1>Welcome to the Home Page</h1>}/>
+                 <Route path="/products" element={<ProductPage/>}/>
+                <Route path="/contact" element={<h1>Contact Us</h1>}/>
+                <Route path="/about" element={<h1>About Us</h1>}/>
+                <Route path="/overview/:id" element={<ProductOverview/>}/>
+                <Route path="/cart" element={<CartPage/>}/>
+                <Route path="/checkout" element={<CheckoutPage/>}/>
+                <Route path="/*" element={<h1>404 Not found</h1>}/>
+               
+                
+                
 
-            <h1>Welcome to the Home Page</h1>
+                
+               </Routes>
         </div>
-    );
+     )
 }
