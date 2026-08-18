7bd76ad2dd3fbace34ce654084b85281fab8bf29
Chathumini <chathuminisenethya246@gmail.com>
2026-08-18 18:49:21 +0530

    route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,55 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import AdminPage from "./pages/adminpage";
 import HomePage from "./pages/homepage.jsx";
 import TestPage from "./pages/test";
 import LoginPage from "./pages/loginPage";
 import RegistrationPage from "./pages/registrationPage.jsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import ForgetPassword from "./pages/forgetPassword.jsx";
 import UserSettings from "./pages/settings.jsx";
 
+import { ProductPage } from "./pages/productPage.jsx";
+import ProductOverview from "./pages/productOverview.jsx";
+import CartPage from "./pages/cart.jsx";
+import CheckoutPage from "./pages/checkout.jsx";
+
 function App() {
-  const [count, setCount] = useState(0);
+    return (
+        <BrowserRouter>
+            <GoogleOAuthProvider
+                clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+            >
+                <div className="w-full h-[100vh]">
+                    <Toaster position="top-right" />
 
-  return (
-    <BrowserRouter>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <div className="w-full h-[100vh]">
-        <Toaster position="top-right" />
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
 
-        <Routes>
-          <Route path="/*" element={<HomePage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/forget-password" element={<ForgetPassword />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/admin/*" element={<AdminPage />} />
-          <Route path="/test" element={<TestPage />} />
-          <Route path="/settings" element={<UserSettings />} />
-        </Routes>
-      </div>
-      </GoogleOAuthProvider>
-    </BrowserRouter>
-  );
+                        <Route path="/register" element={<RegistrationPage />} />
+                        <Route path="/forget-password" element={<ForgetPassword />} />
+                        <Route path="/login" element={<LoginPage />} />
+
+                        <Route path="/products" element={<ProductPage />} />
+                        <Route path="/contact" element={<h1>Contact Us</h1>} />
+                        <Route path="/about" element={<h1>About Us</h1>} />
+                        <Route path="/overview/:id" element={<ProductOverview />} />
+                        <Route path="/cart" element={<CartPage />} />
+                        <Route path="/checkout" element={<CheckoutPage />} />
+
+                        <Route path="/settings" element={<UserSettings />} />
+
+                        <Route path="/admin/*" element={<AdminPage />} />
+                        <Route path="/test" element={<TestPage />} />
+
+                        <Route path="*" element={<h1>404 Not Found</h1>} />
+                    </Routes>
+                </div>
+            </GoogleOAuthProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
